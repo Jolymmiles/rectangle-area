2ca592643424b51d74dac9a08e28705e13b4a640
Валентин <valmak04@gmail.com>
2021-09-26 22:45:59 +0400

    Done 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float x1, x2, y1, y2, x, y, ans;
-        System.out.print("Введите х1:");
         x1 = sc.nextFloat();
-        System.out.print("Введите у1:");
         y1 = sc.nextFloat();
-        System.out.print("Введите х2:");
         x2 = sc.nextFloat();
-        System.out.print("Введите у2:");
         y2 = sc.nextFloat();
         x = x2 - x1;
         y = y2 - y1;
