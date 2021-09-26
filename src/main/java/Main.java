fc9d854785544ed73d4e2a100126d770442660a1
Валентин <valmak04@gmail.com>
2021-09-26 22:42:49 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        float x1, x2, y1, y2, x, y, ans;
+        System.out.print("Введите х1:");
+        x1 = sc.nextFloat();
+        System.out.print("Введите у1:");
+        y1 = sc.nextFloat();
+        System.out.print("Введите х2:");
+        x2 = sc.nextFloat();
+        System.out.print("Введите у2:");
+        y2 = sc.nextFloat();
+        x = x2 - x1;
+        y = y2 - y1;
+        ans = x * y;
+        if ( ans < 0) { System.out.println(-1 * ans);
+        } else {
+            System.out.println(ans);
+        }
+
     }
 }
